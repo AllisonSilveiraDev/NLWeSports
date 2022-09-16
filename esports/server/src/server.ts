1a7cad1d23cb0845f3a97d1f32980674bc603422
Allison Silveira <allisonsilveiradev@gmail.com>
2022-09-15 22:25:21 -0300

    commit 4

--- esports/server/src/server.ts
+++ esports/server/src/server.ts
@@ -1,15 +1,59 @@
 import express from 'express'
 
+import { PrismaClient} from '@prisma/client'
+
 const app = express()
 
-app.get('/ads', (req, res) => {
-  return res.json([
-    { id: 1, name: 'Anuncio 1' },
-    { id: 2, name: 'Anuncio 2' },
-    { id: 3, name: 'Anuncio 3' },
-    { id: 4, name: 'Anuncio 4' },
-    { id: 5, name: 'Anuncio 5' }
-  ])
+const prisma = new PrismaClient({
+  log:['query']
+})
+
+app.get('/games', async (req, res) => {
+  const games = await prisma.game.findMany({
+    include:{
+      _count:{
+        select:{
+          ads: true,
+        }
+      }
+    }
+  })
+
+  return res.json(games)
+});
+
+app.post('/ads', (req, res) => {
+  return res.status(201).json([])
+});
+
+app.get('/games/:id/ads', async (req, res) => {
+  const gameId = req.params.id;
+
+  const ads = await prisma.ad.findMany({
+    select:{
+      id:true,
+      name:true,
+      weekDays:true,
+      useVoiceChannel:true,
+      yearsPlaying:true,
+      hourStart:true,
+      hourEnd:true,
+    },
+    where:{
+      gameId: gameId
+    },
+    orderBy:{
+      creatAt:'desc',
+    }
+  })
+
+  return res.json(ads)
+})
+
+app.get('/ads/:id/discord', (req, res) => {
+  //const adId = req.params.id;
+
+  return res.json([])
 })
 
 app.listen(3333)
